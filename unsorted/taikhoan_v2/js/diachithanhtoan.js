3feddcc83943b074753ec95431b1fc0aa5b1b026
Dan9Gooseman <dangerousopticgaming@gmail.com>
2023-11-13 14:44:25 +0700

    Pham Quang Dang them file home, them file fix dangkydangnhap, taikhoan cua Hoang Thgit commit

---
+++ unsorted/taikhoan_v2/js/diachithanhtoan.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var editButton = document.getElementById("editButton");
+
+  if (editButton) {
+    editButton.addEventListener("click", function () {
+      // Replace "/duong-dan-cua-trang-sua" with the actual path to your edit page
+      window.location.href = "../suadiachi/suadiachi.html";
+    });
+  }
+});

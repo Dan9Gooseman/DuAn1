512a40900f021fda2ef6039026b28590394d9202
Dan9Gooseman <dangerousopticgaming@gmail.com>
2023-11-19 08:09:15 +0700

    PQD chinh sua lai file unsorted

---
+++ View/page/notfound404.php
@@ -0,0 +1,15 @@
+<section class="page-404">
+	<div class="container-404">
+		<div class="col-sm-10  text-center">
+			<div class="four_zero_four_bg">	
+			</div>
+			<div class="contant_box">
+				<h3 class="h2">
+					Có vẻ bạn đã đi lạc về thời đồ đá
+				</h3>
+				<p>Trang bạn tìm kiếm không tồn tại</p>
+				<a href="index.php?act=" class="link">Go to Home</a>
+			</div>
+		</div>
+	</div>
+</section>
